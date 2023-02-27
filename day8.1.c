0942d5b94d7aabac74bf9d1f57344d37bc6cd1e7
dhairya6570 <126230153+dhairya6570@users.noreply.github.com>
2023-02-27 02:01:09 -0500

    Add files via upload

---
+++ day8.1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main (){
+	
+	int a , b ;
+	
+	printf("Write a number A :");
+	scanf("%d",&a);
+	
+	printf("Write a number B :");
+	scanf("%d",&b);
+	
+	if(a<b){
+		
+		printf("min number is A ");
+	}
+	else{
+		printf("min number is B ");
+	}
+	
+	return 0;
+}
